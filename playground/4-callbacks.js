307e1be4bb32051a1202dabc0855680e7994cf20
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-02-13 12:40:12 -0800

    add 4-callback.js

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,14 +1,26 @@
-const geocode = (address, callback)=> {
-    setTimeout(()=>{
-        const data = {
-            latitude:0,
-            longitude:0
-        }
-        callback(data)
+// const geocode = (address, callback)=> {
+//     setTimeout(()=>{
+//         const data = {
+//             latitude:0,
+//             longitude:0
+//         }
+//         callback(data)
 
+//     }, 2000)
+// }
+
+// geocode('Philade', (data)=> {
+//     console.log(data);
+// })
+
+
+const add = (a, b , callback)=> {
+    setTimeout(()=> {
+        callback (a+b)
     }, 2000)
 }
 
-geocode('Philade', (data)=> {
-    console.log(data);
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
 })
