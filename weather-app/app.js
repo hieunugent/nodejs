fcfc13bb456903503a55a09929f49cb9a42ce088
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-02-14 20:17:27 -0800

    add weather capp

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,25 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const address = process.argv[2]
+
+if (!address){
+ console.log();
+}else{
+    geocode(address, (error, {location})=> {
+        if (error){
+            return console.log(error);
+        }
+    
+        forecast(location, (error, forcastData) => {
+            if(error){
+                return   console.log(error)
+            }
+            console.log(location);
+            console.log(forcastData)
+          })
+    })
+}
 
 
-
-
-geocode("Anaheim", (error, data)=> {
-    console.log('Error', error);
-    console.log('Data', data);
-    forecast(data.location, (error, data) => {
-        console.log('Error', error)
-        console.log('Data', data)
-      })
-})
 
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -3,17 +3,17 @@ const request = require('request')
 
 const geocode = (address, callback)=> {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1IjoiaGlldW51Z2VudCIsImEiOiJja2t6Mzg4MXYwY3ZuMm5wNWhpM3E2cG02In0.4Yzl6Z_fB6Q2ZUnHZR5jdA"
-    request({url:url, json:true}, (error, response)=> {
+    request({url, json:true}, (error, {body})=> {
         if(error){
             callback('Unable to connect to location services!', undefined)
         }
-        else if (response.body.features.length===0){
+        else if (body.features.length===0){
             callback('Unable to find the location. Try another search.', undefined)
         }else{
             callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude:response.body.features[0].center[1],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[0],
+                longitude:body.features[0].center[1],
+                location: body.features[0].place_name
             })
         }
     })
