162487a4446ad22a797018461c4411c18f95c975
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-02-16 10:56:45 -0800

    add error page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,57 @@
 
-
+const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
 console.log(__dirname);
-console.log(__filename);
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../template/views')
+const partialPath = path.join(__dirname,'../template/partials' )
 
 const app = express()
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPath)
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res)=> {
-    res.send('<h1>Hello express<h1>')
+    res.render('index' ,{
+        title :'Weather',
+        name:'Henry Nugent'
+
+    })
 })
 app.get('/help', (req, res)=> {
-    res.send([{name: "Henry"}, {name:"Rynnie"}])
+    res.render('help' ,{
+        title :'HELP',
+         helpText = "This is the line of help text"
+
+    })
+})
+app.get("/help/*", (req, res)=> {
+    res.render("404",{
+        title:'404',
+        name:'Henry Nguyen',
+        errorMessage:"help artical does not found"
+    })
 })
 app.get('/about', (req, res)=> {
-    res.send('<title> About </title>')
+    res.render('about' ,{
+        title :'About',
+        name:"Henry Nguyen"
+
+    })
 })
 app.get('/weather', (req, res)=> {
     res.send({
         forecast: "snow wet",
         location:"Anaheim"
+    })
+})
+app.get('*', (req, res)=> {
+    res.render('404', {
+        title:'404',
+        name: 'Henry Nugent',
+        errorMessage:'Page is Not found'
     })
 })
 app.listen(3000, ()=> {
