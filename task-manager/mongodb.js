143072cac94d7be58c40c3d1a3d834df3f4edc44
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-02-18 19:55:02 -0800

    add Mongo Read

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -2,7 +2,7 @@ const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName='task-manager'
-
+//CRUD
 
 MongoClient.connect(connectionURL, {useNewUrlParser:true, useUnifiedTopology: true}, (error, client)=> {
     if (error){
@@ -57,4 +57,26 @@ MongoClient.connect(connectionURL, {useNewUrlParser:true, useUnifiedTopology: tr
     //     }
     //     console.log(results.ops);
     // })
+
+    // db.collection('users').findOne({name:'Hieu', age: 20}, (error, user)=>{
+    //     if(error){
+    //         return console.log('can not fetch user in data');
+    //     }else if (user === null){
+    //         return console.log('user info not found');
+    //     }
+    //     console.log(user);
+    // })
+    // db.collection('users').find({age:27}).toArray((error, users)=> {
+    //     console.log(users);
+    // })
+    db.collection('tasks').findOne({_id: new mongodb.ObjectID("602f0706c653c618bd17d3ae")}, (error, task)=> {
+        if(error){
+            return console.log('Can fetch the task');
+        }
+        console.log(task);
+    })
+    db.collection('tasks').find({completed: true}).toArray((error, tasks)=> {
+        console.log(tasks);
+    })
+
 })
