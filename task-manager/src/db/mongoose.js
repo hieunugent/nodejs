a7c3cd4a19c59048c51bbb476b602c59889cc9fd
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-02-20 17:44:15 -0800

    add mongoose api

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-const { boolean } = require('yargs')
-
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser:true,
@@ -9,32 +8,66 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 const User = mongoose.model('User', {
     name:{
-        type: String
+        type: String,
+        required: true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is not valid')
+            }
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6,
+        trim:true,
+        validate(value){
+            if (value.toLowerCase().includes('password')){
+                throw new Error('Password cannot contain "Password"')
+            }
+        }
+
     },
     age:{
-        type: Number
+        type: Number,
+        default:0,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be positive number')
+            }
+        }
     }
 })
 const Task = mongoose.model('Task',{
     description:{
-        type:String
+        type:String,
+        required:true,
+        trim:true,
     },
     completed:{
-        type: Boolean
+        type: Boolean,
+        default: false,
     }
 })
-const task = new Task({
-    description:'Learn API',
-    completed: true
-})
-task.save().then(()=>{
-    console.log(task);
-}).catch((error)=>{
-    console.log('Error: ', error);
-})
+// const task = new Task({
+//     description:'Learn API',
+//     completed: true
+// })
+// task.save().then(()=>{
+//     console.log(task);
+// }).catch((error)=>{
+//     console.log('Error: ', error);
+// })
 // const me = new User({
 //     name: 'Henry',
-//     age:27
+//     email:'lhdja@',
+//     age:34
 
 // })
 // me.save().then(()=> {
