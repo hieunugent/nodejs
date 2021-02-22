9a05e5b2f9f46016fd245350576083dffae4fb81
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-02-22 13:06:02 -0800

    add read api

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -19,12 +19,53 @@ app.post("/users", (req, res)=> {
      })
 
 })
+app.get("/users", (req, res)=> {
+    User.find({}).then((users)=> {
+        res.send(users)
+    }).catch((e)=> {
+        res.status(500).send()
+    })
+})
+app.get("/users/:id", (req, res)=> {
+    const _id = req.params.id
+    User.findById(_id).then((user)=> {
+        if(!user){
+            return res.status(404).send()
+
+        }
+        res.send(user)
+    }).catch((e)=> {
+        res.status(500).send(e)
+    })
+})
+
 app.post("/tasks", (req, res)=> {
     const task = new Task(req.body)
     task.save().then(()=> {
         res.status(201).send(task)
     }).catch((e)=> {
         res.status(400).send(e)
+    })
+})
+
+app.get("/tasks", (req, res)=>{
+    Task.find({}).then((tasks)=> {
+       
+        res.send(tasks)
+    }).catch((e)=> {
+        res.status(500).send(e)
+    })
+})
+app.get("/tasks/:id", (req,res)=> {
+    const _id = req.params.id
+    Task.findById(_id).then((task)=> {
+        console.log(task);
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e)=> {
+        res.status(500).send()
     })
 })
 
