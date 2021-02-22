1ee92379652d56e5ddf03f215f3a93d179d4206d
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-02-21 20:28:53 -0800

    add mongod model for user

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -6,44 +6,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex:true,
     useUnifiedTopology: true 
 })
-const User = mongoose.model('User', {
-    name:{
-        type: String,
-        required: true,
-        trim:true
-    },
-    email:{
-        type:String,
-        required:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is not valid')
-            }
-        }
-    },
-    password:{
-        type:String,
-        required:true,
-        minlength:6,
-        trim:true,
-        validate(value){
-            if (value.toLowerCase().includes('password')){
-                throw new Error('Password cannot contain "Password"')
-            }
-        }
 
-    },
-    age:{
-        type: Number,
-        default:0,
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be positive number')
-            }
-        }
-    }
-})
 const Task = mongoose.model('Task',{
     description:{
         type:String,
@@ -55,6 +18,9 @@ const Task = mongoose.model('Task',{
         default: false,
     }
 })
+
+
+
 // const task = new Task({
 //     description:'Learn API',
 //     completed: true
