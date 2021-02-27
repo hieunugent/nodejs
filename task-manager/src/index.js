9959ba7fcd997906046ca328dd6a9e4186b4cc4d
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-02-26 19:55:51 -0800

    middle wARE

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -12,9 +12,19 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(userRouter, taskRouter)
 
-
-
-
 app.listen(port, ()=> {
     console.log('connect to server: '+port);
 })
+const bcrypt = require('bcryptjs')
+const myFunction = async() => {
+    const password = 'Red123456'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('Red123456', hashedPassword)
+    console.log(isMatch);
+}
+myFunction()
+
