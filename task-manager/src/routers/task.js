22769725fc46c8b0d373bf02a630675ae22d32a0
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-03-09 17:02:39 -0800

    section 120 filtering data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -31,6 +31,7 @@ router.post("/tasks", auth, async (req, res)=> {
 
 
 // GET /tasks?completed=false
+// GET /task?limit=10&skip=0
 router.get("/tasks", auth, async (req, res)=>{
    const match = {}
    if(req.query.completed){
@@ -40,7 +41,11 @@ router.get("/tasks", auth, async (req, res)=>{
         // const tasks = await Task.find({owner:req.user._id})
         await req.user.populate({
             path:'tasks',
-            match
+            match, 
+            options:{
+                limit: parseInt(req.query.limit), 
+                skip:parseInt(req.query.skip)
+            }
         }).execPopulate()
 
         res.send(req.user.tasks)
