59f2331a5315867625f3164677696cd140b1f7fc
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-03-16 22:51:19 -0700

    adding moment for project

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage } = require('./utils/messages')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -17,8 +18,8 @@ let count = 0
 
 io.on('connection', (socket) =>{
     console.log('New Websocket connection')
-    socket.emit('message','welcome!' )
-    socket.broadcast.emit('message', 'A new user has joined')
+    socket.emit('message',generateMessage('Welcome!!!') )
+    socket.broadcast.emit('message', generateMessage('A new user has joined'))
     socket.on('sendMessage', (message,callback)=> {
         const filter = new Filter()
         if(filter.isProfane(message)){
@@ -32,7 +33,7 @@ io.on('connection', (socket) =>{
         callback()
     })
     socket.on('disconnect', ()=> {
-        io.emit('message','A User has left')
+        io.emit('message',generateMessage('A User has left'))
     })
     // socket.emit('countUpdated', count )
     // socket.on('increment', ()=> {
